1ce7d25f28bd569c6df323cbb2d44c9e797b1b1f
Alex <zlatos.ale@gmail.com>
2024-11-04 17:45:08 +0200

    6

--- index.js
+++ index.js
@@ -2,16 +2,11 @@
 const dropdownIcon = document.querySelector('.dropdown-icon');
 const dropdownContent = document.querySelector('.dropdown-content');
 const overlay = document.querySelector('.overlay');
-
-// Toggle dropdown and background effect when icon is clicked
 dropdownIcon.addEventListener('click', function () {
     const isDropdownVisible = dropdownContent.classList.contains('show');
-
-    // Toggle the 'show' class for dropdown content and overlay visibility
     dropdownContent.classList.toggle('show');
     document.body.classList.toggle('dropdown-active');
 
-    // If the dropdown is visible, make it hidden by adjusting opacity and pointer-events
     if (isDropdownVisible) {
         dropdownContent.style.opacity = '0';
         dropdownContent.style.pointerEvents = 'none';
